f78fdaca524034b33a056256665eb12ea3cda982
theverything <j3ffhorn@gmail.com>
2013-03-02 16:59:16 -0800

    add swag

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'rubygems'
 require 'sinatra'
+require './lib/broem_ipsum'
 
 get '/' do
   erb :index
+end
+
+post '/swag' do
+  bro = BroemIpsum.new(params[:number].to_i)
+  bro.lets_do_this
+  @paragraphs = bro.paragraphs
+  erb :swag
 end
