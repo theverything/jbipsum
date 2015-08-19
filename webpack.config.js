f93ee4723322f7314b5132b3c9eee4f4a735d968
theverything <j3ffhorn@gmail.com>
2015-08-18 23:09:08 -0700

    create prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,56 @@
 var path = require('path');
 var webpack = require('webpack');
+var env = process.argv[2];
+var loaders = [];
+var plugins = [];
+var entry = [];
+var externals = {};
+var devtool;
 
-module.exports = {
-  devtool: 'eval',
-  entry: [
+if (!env) {
+  loaders = loaders.concat([
+    'react-hot'
+  ]);
+  plugins = plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]);
+  entry = entry.concat([
     'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
+    'webpack/hot/only-dev-server'
+  ]);
+  devtool = 'eval';
+}
+
+if (env && env === '--prod') {
+  plugins = plugins.concat([
+    new webpack.optimize.UglifyJsPlugin()
+  ]);
+  externals = {
+    'react': 'React'
+  };
+}
+
+var config = {
+  devtool: devtool,
+  entry: entry.concat(['./src/index']),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins: plugins,
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['react-hot', 'babel'],
+      loaders: loaders.concat(['babel']),
       include: path.join(__dirname, 'src')
     }]
-  }
+  },
+  externals: externals
 };
+
+module.exports = config;
